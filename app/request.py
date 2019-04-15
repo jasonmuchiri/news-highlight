77e922c8146c9f73ed2421064fa49dc9ae95dc59
jasonmuchiri <jasonmkinyua@gmail.com>
2019-04-15 17:21:37 +0300

    Create a function that will process the results and create news objects from the needed elements

--- app/request.py
+++ app/request.py
@@ -4,4 +4,36 @@ from .models import news
 
 News = news.News
 
-apiKey = app.config['NEWS_API_KEY']
+api_key = app.config['NEWS_API_KEY']
+
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+def process_results(news_list):
+
+    news_results = []
+    for news_item in news_list:
+        id = news_item.get('id')
+        title = news_item.get('original_title')
+        description = news_item.get('description')
+
+        if news:
+            news_object = News(id,title,description)
+            news_results.append(news_object)
+
+    return news_results        
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
 @app.route('/')
 def index():
 
+    popular_news = get_news('popular')
+    print(popular_news)
     title = 'Home - Welcome to the best News Highlight website online'
-    return render_template('index.html',title = title)
+    return render_template('index.html',title = title,popular = popular_news)
