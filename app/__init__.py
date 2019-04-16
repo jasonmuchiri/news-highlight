08fda7eb198ae4bd1107791888c65740a3f1a404
jasonmuchiri <jasonmkinyua@gmail.com>
2019-04-17 01:48:05 +0300

    Make changes to the config.py file for efficiency in the functionality

--- app/__init__.py
+++ app/__init__.py
@@ -5,20 +5,19 @@ from config import config_options
 bootstrap = Bootstrap()
 
 def create_app(config_name):
+
     app = Flask(__name__)
 
-    # Creating app configurations
     app.config.from_object(config_options[config_name])
+    config_options[config_name].init_app(app)
 
-    # Initializing Flask Extensions
     bootstrap.init_app(app)
 
-    # Registering the BluePrint
+    # Registering the blueprint
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    # setting the config
-    from .request import configure_request
+    # setting config
+    from .requests import configure_request
     configure_request(app)
-
     return app
